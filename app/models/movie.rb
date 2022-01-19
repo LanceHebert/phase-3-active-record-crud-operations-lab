81a467ee1561a7bbf76ac2f7d1a762dcf99a8161
Lance Hebert <LSUHEBERT@gmail.com>
2022-01-19 09:35:07 -0800

    commit tree 68bfe4eca87c7c99835e141ff76e631d9a49eb55

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,40 @@
 class Movie < ActiveRecord::Base
 
+        def self.create_with_title(title)
+            create(title: title)
+        end
+
+        def self.first_movie
+            self.first
+        end
+        def self.last_movie
+            self.last
+        end
+        def self.movie_count
+            self.all.size
+        end
+        def self.find_movie_with_id(id)
+            self.find(id)
+        end
+        def self.find_movie_with_attributes(attri)
+            self.find_by(attri)
+        end
+
+        def self.find_movies_after_2002
+            self.where("release_date > 2002")
+        end
+
+        def update_with_attributes(attri)
+            self.update(attri)
+        end
+        def self.update_all_titles(title)
+            self.all.update(title: title)
+        end
+        def self.delete_by_id(id)
+            destroy(id)
+        end
+        def self.delete_all_movies
+            self.destroy_all
+        end
+
 end
